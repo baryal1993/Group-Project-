f439a1f412945abfc6aff766b73133ffa9a5ba2f
HAFIZA WAJEEHA NISAR <s224690856@deakin.edu.au>
2024-10-03 19:08:40 +1000

    loading library

--- code.R
+++ code.R
@@ -1,1 +1,10 @@
 library(tidy)
+install.packages("tidyverse")
+library(tidyverse)
+library(RPostgres)
+wrds <- dbConnect(Postgres(),
+                  host='wrds-pgdata.wharton.upenn.edu',
+                  port=9737,
+                  dbname='wrds',
+                  sslmode='require',
+                  user='wajeehanisar')
